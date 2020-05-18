362b952e5b2c9b702c20a160795d5c7e333b9c40
dionaditya <dionjatingaleh@gmail.com>
2020-05-19 02:00:37 +0700

    Base project laravel created

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Contact;
+use Faker\Generator as Faker;
+
+$factory->define(Contact::class, function (Faker $faker) {
+    
+    $colors = collect(range(1, 3))->map(function() use ($faker) {
+        return $faker->colorName;
+    })->toArray();
+
+
+    return [
+        'name' => $faker->name,
+        'phone' => $faker->e164PhoneNumber,
+        'address' => $faker->address,
+        'favorites' => ['colors' => $colors],
+    ];
+});
